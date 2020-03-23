92c1b51396f35eae8c2660a003beae5ed096319c
stephen meckstroth <meckstss@gmail.com>
2020-03-23 00:18:33 -0400

    Simplified code a little bit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # RaspberryPiDashboard
 Web dashboard that displays real-time data from attached sensors on a RaspberryPi
+
+# First install Node-RED
+https://nodered.org/docs/getting-started/raspberrypi
+
+# Install drivers for the SenseHAT
+https://flows.nodered.org/node/node-red-node-pi-sense-hat
+
+# Install Azure-iot-hub
+https://flows.nodered.org/node/node-red-contrib-azure-iot-hub
+
+Once setup, the data will stream straight to Azure!
--- app.py
+++ app.py
@@ -17,23 +17,7 @@ def hello():
     pressure = round(sense.get_pressure(),2)
     temp = round((sense.get_temperature() * (9/5)) + 32,1)
     humidity = round(sense.get_humidity(),2)
-    message = "Pressure: {0} Temp: {1}F Humidity: {2}".format(pressure, temp, humidity)
-    if (temp < 120):
-            color = blue
-    elif (temp > 140):
-            color = red
-    else:
-            temp = white
-
-    sense.set_rotation(rotation[2])
-    sense.show_message(message, scroll_speed=0.07, text_colour=color)
-    sleep(0.1)
-    sense.set_pixel(0, 0, (0, 0, 255))
-    sense.set_pixel(0,7, blue)
-    sense.set_pixel(7,0,blue)
-    sense.set_pixel(7,7,blue)
-    sleep(0.1)
-    sense.clear((255,255,255))
+    
     html = """<html>
             <head>
             <script>
