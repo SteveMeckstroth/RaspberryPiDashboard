cf2afe45c07e8e0d7e80a4545875f8f6b59207e0
stephen meckstroth <meckstss@gmail.com>
2020-03-22 23:43:49 -0400

    Added data from sensors

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
+from sense_hat import SenseHat
+from time import sleep
 from flask import Flask
 app = Flask(__name__)
+sense = SenseHat()
 
 @app.route("/")
 def hello():
-        return """
+    pressure = round(sense.get_pressure(),2)
+    temp = round((sense.get_temperature() * (9/5)) + 32,1)
+    humidity = round(sense.get_humidity(),2)
+    message = f"Pressure: {pressure} Temp: {temp}F Humidity: {humidity}"
+    if (temp < 120):
+            color = blue
+    elif (temp > 140):
+            color = red
+    else:
+            temp = white
+
+    sense.set_rotation(rotation[2])
+    sense.show_message(message, scroll_speed=0.07, text_colour=color)
+    sleep(0.1)
+    sense.set_pixel(0, 0, (0, 0, 255))
+    sense.set_pixel(0,7, blue)
+    sense.set_pixel(7,0,blue)
+    sense.set_pixel(7,7,blue)
+    sleep(0.1)
+    sense.clear((255,255,255))
+    return f"""
 <html>
 <head>
 <script>
@@ -13,7 +36,10 @@ setTimeout(function() {
 </script>
 </head>
 <body>
-<div><h1>Hello</h1></div>
+<div><p><h1>Temp: {temp}</h1></p>
+    <p><h1>Pressure: {pressure}</h1></p>
+    <p><h1>Humidity: {humidity}</h1></p>
+</div>
 </body>
 </html>
 
